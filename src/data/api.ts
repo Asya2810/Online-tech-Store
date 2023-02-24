b8759c8b760e56cb506df74f5994e0f83acd4b76
Asya2810 <118177334+Asya2810@users.noreply.github.com>
2023-02-24 17:56:46 +0600

    Add files via upload

---
+++ src/data/api.ts
@@ -0,0 +1,47 @@
+import ProductInfo from "../components/ProductInfo";
+
+export const Product = [
+    {
+        id: 1,
+        productName: 'Laptop',
+        model: "HP 8803",
+        OS: "Windows 11",
+        isActive: true,
+        about: "Memory & Storage: Memory is 8GB high-bandwidth RAM to smoothly run multiple applications and browser tabs all at once. Hard Drive is 256GB PCIe Solid State Drive which allows to fast bootup and data transfer",
+        url: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06750768.png",
+        price: 1670.99,
+        conditon: 'Factory new'
+    },
+    {
+        id: 2,
+        productName: 'Computer',
+        model: "F1774W",
+        OS: "Windows 10",
+        isActive: true,
+        about: "Intel® Core™ i5-1235U (up to 4.4 GHz with Intel® Turbo Boost Technology, 12 MB L3 cache, 10 cores, 12 threads)",
+        url: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c07937820.png",
+        price: 2280.99,
+        conditon: 'Factory New'
+    },
+    {
+        id: 3,
+        productName: 'Headphones',
+        model: "Logitech H390",
+        isActive: true,
+        about: "Optimum Comfort: PC headphones with plush, padded headband and earcups, Enhanced Audio: Experience premium sound quality. System requirements: Windows Vista, Windows 7, Windows 8, Windows 10 or later, Chrome OS or  MAC OS X (10.2.8 or later), with USB-A port",
+        url: "https://resource.logitech.com/w_692,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/headsets/h390-usb-headset/new-gallery/h390-gallery-offwhite-1-1.png?v=1",
+        price: 19.99,
+        conditon: 'Used'
+    },
+    {
+        id: 4,
+        productName: 'Keyboard',
+        model: "MX MECHANICAL MINI FOR MAC",
+        isActive: true,
+        about: "Compatible with Logi Bolt USB Receiver only (not included) 16Not compatible with Logitech Unifying USB Receiver technology, Compatible with Logitech Flow enabled mouse, Logi Bolt wireless products will not pair with other Logitech USB receivers", 
+        url: "https://resource.logitech.com/w_692,c_limit,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/keyboards/mx-mechanical-mini-mac/gallery/pale-grey/mx-mechanical-mini-for-mac-keyboard-top-view-pale-grey-us.png?v=1",
+        price: 119.99,
+        conditon: 'FactoryNew'
+    }
+];
+
