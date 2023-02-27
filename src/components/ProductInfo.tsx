d3eac90a91e7166a46ebc0d9466cd997f2e66add
Asya2810 <118177334+Asya2810@users.noreply.github.com>
2023-02-27 16:11:31 +0600

    Update ProductInfo.tsx

--- src/components/ProductInfo.tsx
+++ src/components/ProductInfo.tsx
@@ -1,18 +1,27 @@
-import { Product } from "../data/api";
+import { useState } from "react";
+import { product } from "./data/api";
 
 export default function ProductInfo (props:any) {
     let {product} = props;
+    const [isDetailed, setDetailed] = useState(false);
 
     return(
         <>
-            <div>
-                <h1>{product.productName} {product.Model}</h1>
-                <h2>{product.OS}</h2>
-                <h3>${product.price}</h3>
-                <h4>{product.conditon}</h4>
-                <img src={product.url} alt="laptop pic" className="picture"/>
-                <p>{product.about}</p>
-            </div>
+        {product.isActive && (
+            <>
+                <div>
+                    <h1>{product.productName} {product.Model}</h1>
+                    <h2>{product.OS}</h2>
+                    <h3>${product.price}</h3>
+                    <h4>{product.conditon}</h4>
+                    <img src={product.url} alt="laptop pic" className="picture"/>
+                    <button onClick={()=>setDetailed(!isDetailed)} className="btn btn-primary" id="right-panel-link">
+                        {isDetailed ? "" : ""}
+                        Details
+                    </button>
+                    {isDetailed && <p>{product.about}</p>}
+                </div>
+            </>
+        )}
         </>
     );
-}
